5a891e711c4e38c01e7d0fb3194e72f1f2104709
arianimacedo <115736820+arianimacedo@users.noreply.github.com>
2022-11-23 19:22:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,36 @@ $(document).on('ready', function() {
    
   year.innerHTML = new Date(). getFullYear()
 
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept' : 'application/json'
+    }
+
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Agradecemos o contato!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error
+          ["message"]).join(", ")
+        } else {
+          status.innerHTML = "Opa! Houve um erro no envio do seu formulário"
+        }
+      })
+    }
+  }).catch(error => {
+          status.innerHTML = "Opa! Houve um erro no envio do seu formulário"
+  });
+}
+
+form.addEventListener("submit", handleSubmit)
